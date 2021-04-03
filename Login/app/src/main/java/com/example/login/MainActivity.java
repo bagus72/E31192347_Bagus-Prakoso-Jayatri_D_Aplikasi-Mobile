4d6ff71362ca49d2471b015aea0a96ca95b9db17
bagus72 <65766613+bagus72@users.noreply.github.com>
2021-04-03 20:33:14 +0700

    Tugas Login dan Intent

--- Login/app/src/main/java/com/example/login/MainActivity.java
+++ Login/app/src/main/java/com/example/login/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.login;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v4.app.RemoteActionCompatParcelizer;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Username, Password;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = (EditText) findViewById(R.id.Username);
+        Password = (EditText) findViewById(R.id.Password);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usernameKey = Username.getText().toString();
+                String passwordKey = Password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                    //jika login berhasil
+                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, PilihGambar.class);
+                    MainActivity.this.startActivity(intent);
+                    finish();
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password Anda salah!")
+                            .setNegativeButton("Retry", null).create().show();
+                }
+            }
+
+        });
+
     }
 }
+
